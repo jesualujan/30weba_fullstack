28b4cda02e5b90d5f0e9295e1464fab678db6908
jesualujan <lujan.oficial@gmail.com>
2024-08-12 20:08:13 -0600

    ✨ creamos la vista para cada producto por su id

--- ecommerce/pages/product/[id].js
+++ ecommerce/pages/product/[id].js
@@ -8,6 +8,9 @@
 import React from 'react'
 import {useRouter} from 'next/router'
 import {data} from '../../utils/data'
+import { 
+    Container, SimpleGrid, Flex, Image, Heading, Stack, Box, Text, 
+    useColorModeValue, Button } from '@chakra-ui/react'
 
 const ProductPage = () => {
     const router = useRouter() // es un hook de next 
@@ -20,9 +23,54 @@ const ProductPage = () => {
     }
 
   return (
-    <div>
-        {`Product: id - ${id}`}
-    </div>
+    <Container maxW={'container.xl'} mt={2}>
+        <SimpleGrid columns={[1,2]} spacing={2}>
+            <Flex>
+                <Image 
+                src={`/images/${product.image}`}
+                fit={'cover'}
+                alt={product.title}
+                align={'center'}
+                h={'100%'}
+                w={{base: '100%', sm:'400px', lg:'500px'}}
+                rounded={'md'}
+                />
+            </Flex>
+            <Stack spacing={{base: 6, md:10}}>
+                <Box>
+                    <Heading
+                    fontWeight={600}
+                    fontSize={{base: '2xl', sm: '4xl', lg: '4xl'}}
+                    >
+                        {product.title}
+                    </Heading>
+                    <Text
+                    color={useColorModeValue('gray.900', 'gray.400')}
+                    fontWeight={350}
+                    fontSize={'2xl'}
+                    >
+                       {`$${product.price} USD`}
+                    </Text>
+                </Box>
+                <Text
+                 color={useColorModeValue('gray.500', 'gray.700')}
+                 fontSize={'lg'}
+                >
+                {product.description}
+                </Text>
+                <Flex alignItems={'end'}>
+                    <Button
+                    rounded={'md'} w={'full'} mt={8} size={'lg'} py={'7'}
+                    bg={useColorModeValue('gray.900', 'gray.50')}
+                    color={useColorModeValue('white', 'gray.900')}
+                    textTransform={'uppercase'}
+                    >
+                        Add to cart
+                    </Button>
+                </Flex>
+            </Stack>
+        </SimpleGrid>
+    </Container>
   )
 }
 
