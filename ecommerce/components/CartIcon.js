53fd3de00860d85f287737d0e1ef2f47ce1d8c4a
jesualujan <lujan.oficial@gmail.com>
2024-08-19 20:10:27 -0600

    ✨ creamos el componente CartIcon

---
+++ ecommerce/components/CartIcon.js
@@ -0,0 +1,36 @@
+import React , {useContext} from 'react'
+import { CartContext } from '../context/Cart'
+import { useColorMode, IconButton, Box, Text } from '@chakra-ui/react'
+
+const CartIcon = () => {
+    const { cart } = useContext(CartContext) // consumimos el contexto
+    const { colorMode } = useColorMode()
+    const hoverColor = {light: "gray.800", dark: "gray.200"}
+    const iconColor =  {light: "gray.600", dark: "gray.300"}
+    const fontColor = {light: "gray.800", dark: "gray.100"}
+
+  return (
+    <Box position='relative'>
+        <IconButton aria-label='cart'
+        icon={<Text fontSize={'2xl'}>🛒</Text>}
+        color = {iconColor[colorMode]}
+        _hover={{color: hoverColor[colorMode], transform: 'scale(1.1)'}} />
+        {cart.length > 0 && (
+                <Box
+                position={"absolute"}
+                top={0}
+                right={0}
+                bg={hoverColor[colorMode]}
+                color={fontColor[colorMode]}
+                rounded="sm"
+                p={1}
+                >
+                <Text fontWeight="bold">{cart.length}</Text>
+                </Box>
+            )
+        }
+    </Box>
+  )
+}
+
+export default CartIcon
