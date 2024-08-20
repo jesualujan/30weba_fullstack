4e22220016fe5962344a67c618d954bb3bc50437
jesualujan <lujan.oficial@gmail.com>
2024-08-19 19:43:56 -0600

    ✨ creamos el contexto del carrito de compras

---
+++ ecommerce/context/Cart.js
@@ -0,0 +1,54 @@
+import { useReducer, createContext } from 'react'
+
+// iniciamos un estado para el carrito de compras
+  const initialState = {
+    cart: [],
+  }
+
+  const cartReducer = (state, action) => {
+      // AGREGAR AL CARRITO: "ADD_TO_CART"
+      // REMOVER DEL CARRITO: "REMOVE_FROM_CART"
+      switch(action.type){
+        case "ADD_TO_CART":
+            return {...state, cart:[...state.cart, action.payload]}
+            case "REMOVE_FROM_CART":
+                return {...state, 
+                    cart: state.cart.filter(item => item._id !== action.payload)
+                }
+                default:
+                    return state
+      }
+  }
+
+  // creamos el contexto 
+   const CartConext = createContext() // crear el contexto
+
+   // creamos el proveedor del contexto podemos usar 
+   // el componente Context.Provider 
+
+   const CartProvider = ({children}) =>{
+     // datos a compartir con otros componentes 
+     const [state, dispatch] = useReducer(cartReducer, initialState)
+     const addToCart = (item) =>{
+        dispatch({type: "ADD_TO_CART", payload: item})
+     }
+
+     const removeFromCart = (item) => {
+        dispatch({type: "REMOVE_FROM_CART", payload: item._id})
+     }
+
+     return (
+        <CartConext.Provider
+        // value paso los datos que quiero compartir con otros componentes
+         value={
+            {state, addToCart, removeFromCart}
+         }
+        >
+         {/* componentes hijos */}
+         {children}
+        </CartConext.Provider>
+     )
+
+   }
+
+   export { CartConext, CartProvider}
