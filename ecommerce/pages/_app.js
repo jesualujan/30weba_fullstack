eee359abddd705ddd3cf087f1e7b19e367048f0e
jesualujan <lujan.oficial@gmail.com>
2024-08-19 19:44:12 -0600

    ✨ importar el proveedor del contexto

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from "../components/Layout"
+import { CartProvider } from '../context/Cart'
 
 export default function App({ Component, pageProps }) {
   return (
   <ChakraProvider>
+    <CartProvider>
     <Layout>  
-    <Component {...pageProps} />
+     <Component {...pageProps} />
     </Layout>
+    </CartProvider>
   </ChakraProvider>
   )
 }
