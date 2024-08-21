f06472987292d10b794fc0116756e06f15c7e543
jesualujan <lujan.oficial@gmail.com>
2024-08-21 17:08:00 -0600

    💄 agregue estilos

---
+++ ecommerce/pages/cart.js
@@ -0,0 +1,49 @@
+import React, {useContext} from 'react'
+import CartItem from '../components/CartItem'
+import { CartContext } from '../context/Cart'
+import { 
+    Box,
+    Text,
+    Flex,
+    Heading,
+    Stack,
+} from 'chakra-ui/react'
+import OrderSummary from '../components/OrderSummary'
+
+const CartPage = () => {
+    const { cart } = useContext(CartContext)
+    const calculateTotal = () => {
+        // esta es la función de reducción, que se ejecuta en cada momento del arreglo
+        // acc -> es el acumulador que almacena el resultado parcial,
+        // item -> es el elemento actual en el recorrido del arreglo 
+        // 0 -> es el valor inicial del acumulador
+        return cartItems.reduce((acc,item) => acc + item.price, 0)
+    }
+  return (
+    <Box
+    maxW={{base:'3xl', lg:'5xl'}}
+    mx="auto"
+    px={{base: '4', md: '8', lg: '12'}}
+    py={{base: '4', md: '8', lg: '12'}}
+ >
+       <Text fontSize='xl' fontWeight='bold'> Your Cart is empty </Text>
+       <Stack
+                as="section"
+                spacing={{base: '9', lg:'14'}}
+                direction={{base: 'column', lg: 'row'}}
+                align={{lg: 'flex-start'}}
+                >
+          <Stack flex="2" spacing={{base: 6, lg:'10'}}>
+          <Heading as="h1" size="2xl">Shopping Cart</Heading>
+            <CartItem />
+          </Stack>
+
+          <Flex direction="column" align="center" flex="1">
+                            <OrderSummary total={calculateTotal(cart)}/>
+            </Flex>
+       </Stack>
+   </Box>
+  )
+}
+
+export default CartPage
