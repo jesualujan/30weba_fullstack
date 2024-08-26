8e5fdb47dc88b00707174e3aa717748e9cb225a2
jesualujan <lujan.oficial@gmail.com>
2024-08-26 15:54:08 -0600

    ✨ hice cambios en la conexión

--- ecommerce/utils/db.js
+++ ecommerce/utils/db.js
@@ -1,44 +1,42 @@
-// conexión a la base de datos
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const connection = {};
+const connection = {}
 
 async function connect() {
   if (connection.isConnected) {
     console.log("Conexión Exitosa 🚀");
     return;
   }
-  if (mongoose.connections.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState;
-    if (connection.isConnected === 1) {
+  if(mongoose.connection.length > 0){
+    connection.isConnected = mongoose.connections[0].readyState
+    if(connection.isConnected === 1)
       console.log("Usuario ya conectado 💻");
-      return;
-    }
-    await mongoose.disconnect();
+    return
   }
 
   const db = await mongoose.connect(process.env.MONGODB_URI)
   console.log("nueva conexión 😃");
-  connection.isConnected = db.connections[0].readyState;
+  connection.isConnected = db.connections[0].readyState
 }
 
 async function disconnect() {
-  if (connection.isConnected) {
-    if (process.env.NODE_ENV === "production") {
-      await mongoose.disconnect();
-      connection.isConnected = false;
+  if(connection.isConnected){
+    if(process.env.NODE_ENV === 'production'){
+        await mongoose.disconnect()
+        connection.isConnected = false
     } else {
       console.log("conectado 💻");
     }
   }
 }
 
-function convertDocToObj() {
-
+function convertDocToObj(doc) {
+  doc._id = doc._id.toString() // convierte el valor de la propiedad "_id" a una cadena o string
+  doc.createdAt = doc.createdAt.toString() // convierte el valor de la propiedad "createdAt" a una cadena o string
+  doc.updatedAt = doc.updatedAt.toString() // convierte el valor de la propiedad "updatedAt" a una cadena o string
+  return doc // devuelve el objeto modificado
 }
 
-
-
-const db = { connect, disconnect, convertDocToObj}
+const db = { connect, disconnect, convertDocToObj }
 
 export default db
