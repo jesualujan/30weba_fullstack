3c0e7e2da1a1f6dc58f26c7ef81889343667bf98
jesualujan <lujan.oficial@gmail.com>
2024-08-28 17:17:48 -0600

    ✨ usamos crypto para generar nuestro secret

--- ecommerce/utils/generateJwtSecret.js
+++ ecommerce/utils/generateJwtSecret.js
@@ -10,5 +10,4 @@ vamos a la terminal y ejecutamos lo siguiente:
    node utils/generateJwtSecret.js
   esto nos va a generar un secret:
   31bf2801206a012a7f289982ff7fb94c75ecdea2f3e17a324ce2a44c21f4991358934dfad6dcaee63f24d50f183fab825f529f65716058316223ad5f74a6ee60
-
 */
